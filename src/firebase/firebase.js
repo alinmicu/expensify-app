19e9f515737ea8a1a10bac67655f15791fa48ad8
Alin <micualin24@gmail.com>
2021-09-16 21:42:54 +0300

    Add private routess

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,7 +12,9 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database(); 
-export { firebase, database as default };
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export { firebase, googleAuthProvider, database as default };
 
 // // child_removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,22 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
-import thunk from 'redux-thunk';
-import { compose } from "redux";
+import authReducer from '../reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// Store creation
 export default () => {
-    const store = createStore(
-        combineReducers({
-        expenses: expensesReducer,
-        filters: filtersReducer
-        }),
-        composeEnhancers(applyMiddleware(thunk))
-        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer,
+      auth: authReducer
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
   return store;
 };
-  
